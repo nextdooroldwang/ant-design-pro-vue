11eb5892f8c2163b978c7d4d1fe045accc80a778
王蒙 <wangmeng@wangmengdeMini.lan>
2019-03-19 15:26:29 +0800

    mobingi text

--- src/components/page/GlobalFooter.vue
+++ src/components/page/GlobalFooter.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="footer">
     <div class="links">
-      <a href="https://pro.ant.design/" target="_blank">Pro 首页</a>
-      <a href="https://github.com/ant-design/ant-design-pro" target="_blank">
+      <a target="_self">首页</a>
+      <a href="https://github.com/mobingilabs" target="_blank">
         <a-icon type="github"/>
       </a>
       <a href="https://ant.design/">Ant Design</a>
@@ -10,8 +10,8 @@
     </div>
     <div class="copyright">
       Copyright
-      <a-icon type="copyright"/>
-      2018 <span>白鹭学园技术组出品</span>
+      <a-icon type="copyright"/>2019
+      <span>Mobingi研发组出品</span>
     </div>
   </div>
 </template>
@@ -27,29 +27,29 @@ export default {
 </script>
 
 <style lang="less" scoped>
-  .footer {
-    padding: 0 16px;
-    margin: 48px 0 24px;
-    text-align: center;
+.footer {
+  padding: 0 16px;
+  margin: 48px 0 24px;
+  text-align: center;
 
-    .links {
-      margin-bottom: 8px;
+  .links {
+    margin-bottom: 8px;
 
-      a {
-        color: rgba(0, 0, 0, .45);
+    a {
+      color: rgba(0, 0, 0, 0.45);
 
-        &:hover {
-          color: rgba(0, 0, 0, .65);
-        }
+      &:hover {
+        color: rgba(0, 0, 0, 0.65);
+      }
 
-        &:not(:last-child) {
-          margin-right: 40px;
-        }
+      &:not(:last-child) {
+        margin-right: 40px;
       }
     }
-    .copyright {
-      color: rgba(0, 0, 0, .45);
-      font-size: 14px;
-    }
   }
+  .copyright {
+    color: rgba(0, 0, 0, 0.45);
+    font-size: 14px;
+  }
+}
 </style>
